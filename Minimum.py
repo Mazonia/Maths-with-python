0216b9ac94a792fdde255ec5b5ecd067ebd0b103
Mazonia <153426409+Mazonia@users.noreply.github.com>
2024-05-02 06:51:58 -0400

    Add files via upload

---
+++ Minimum.py
@@ -0,0 +1,5 @@
+num1 = int(input("Enter a first number"))
+num2 = int(input("Enter a second number"))
+num3 = int(input("Enter a third number"))
+minimum = min(num1, num2, num3)
+print(minimum)
---
+++ POSITIVE NEGATIVE.py
@@ -0,0 +1,8 @@
+value = float(input("Enter a number of your choice"))
+if value < 0:
+    print("The value is negative")
+elif value == 0:
+    print("The value is zero")
+    print("The value is neither positive or negative")
+else:
+    print("The value is positive")
